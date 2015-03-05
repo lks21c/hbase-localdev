9c3dcdfd65f36cdce97b90c811a3d2da4429ce04
kwangsik <lks21c@gmail.com>
2015-03-05 18:31:18 +0900

    First Commit

---
+++ src/test/java/MyHBaseIntegrationTest.java
@@ -0,0 +1,39 @@
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.CellUtil;
+import org.apache.hadoop.hbase.HBaseTestingUtility;
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.HTableInterface;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MyHBaseIntegrationTest {
+	private static HBaseTestingUtility utility;
+	byte[] CF = "CF".getBytes();
+	byte[] QUALIFIER = "qualifier".getBytes();
+
+	@Before
+	public void setup() throws Exception {
+		utility = new HBaseTestingUtility();
+		utility.startMiniCluster();
+	}
+
+	@Test
+	public void testInsert() throws Exception {
+		HTableInterface table = utility.createTable(Bytes.toBytes("MyTest"),
+				Bytes.toBytes("CF"));
+		
+		Put put = new Put("rowkey".getBytes());
+		put.add(CF, QUALIFIER, Bytes.toBytes("1"));
+		table.put(put);
+		
+		Result result = table.get(new Get("rowkey".getBytes()));
+		result.getColumnCells(CF, QUALIFIER);
+		Cell cellValue = result.getColumnLatestCell(Bytes.toBytes("CF"), QUALIFIER);
+		String str = Bytes.toString(CellUtil.cloneValue(cellValue));
+		Assert.assertEquals("1", str);
+	}
+}
